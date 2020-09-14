8337dc2eec1c3c4ac9bac9b475d9b21417da0806
Taiyo L Baniecki <tlb416@nyu.edu>
2020-09-14 23:19:32 +0900

    Commit initial homework

---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    public float speed = 1f;
+    public GameObject mainCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0, speed * Time.deltaTime, 0);
+            mainCamera.transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(0, -speed * Time.deltaTime, 0);
+            mainCamera.transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-speed * Time.deltaTime, 0, 0);
+            mainCamera.transform.Translate(-speed * Time.deltaTime, 0, 0);
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0);
+            this.transform.localScale = new Vector3(0.1f, 0.1f, 1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(speed * Time.deltaTime, 0, 0);
+            mainCamera.transform.Translate(speed * Time.deltaTime, 0, 0);
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0);
+            this.transform.localScale = new Vector3(-0.1f,0.1f,1);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAnimationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationManager : MonoBehaviour
+{
+    public Sprite[] playerAnimation;
+    private int frame = 1;
+    public float animationSpeed = 1;
+    private float count = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count < animationSpeed) count += Time.deltaTime;
+        else{
+            count = 0;
+            if (frame < 6) frame++;
+            else frame = 1;
+            this.GetComponent<SpriteRenderer>().sprite = playerAnimation[frame-1];
+        }
+    }
+}
